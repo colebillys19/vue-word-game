2c1118496201aa6bddcac9acdc0d4bba630845dc
Billy Cole <billycole@billys-mbp.mynetworksettings.com>
2023-03-30 14:21:37 -0400

    indicate focused column

--- src/components/ColumnIndicators.vue
+++ src/components/ColumnIndicators.vue
@@ -1,58 +1,39 @@
 <script setup>
-// import { useBoardStore } from '@/stores/board'
-// const boardStore = useBoardStore()
+import { computed } from 'vue'
+import { useBoardStore } from '@/stores/board'
+
+const boardStore = useBoardStore()
+
+const indicatorDataArr = computed(
+  () => new Array(26).fill()
+    .map((v, i) => ({
+      isVisible: boardStore.focusedIndices[1] === i, key: String.fromCharCode(i + 97)
+    }))
+)
 </script>
 
 <template>
   <div>
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
-    <span />
+    <span v-for="data in indicatorDataArr" :key="data.key" :style="data.isVisible ? 'visibility: visible;' : 'visibility: hidden;'" />
   </div>
 </template>
 
 <style scoped>
 div {
-  position: absolute;
   left: 0;
+  position: absolute;
   top: 0;
   z-index: -1;
 }
 
 span {
+  background-color: rgba(248, 89, 0, 0.3);
+  display: inline-block;
   height: 624px;
   width: 23px;
-  display: inline-block;
 }
 
 span:not(:last-of-type) {
   margin-right: 1px;
-}
-
-span:nth-of-type(odd) {
-  background-color: pink;
 }
 </style>
